6bd9d17b80a5bab3ba7aa0cf836321a029f0cc59
Muneeb <muneeb.official.686332@hotmail.com>
2022-12-12 15:25:19 +0500

    Equal button done

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
+import {infixToPostfix,calculate} from './infixEvaluation.js';
+
 let button = document.querySelectorAll(".items");
 let input = document.getElementById("input");
 let clear = document.getElementById("clear");
 let equal = document.getElementById("equal");
 let value=""
 
+
 clear.addEventListener("click",()=>{
     input.setAttribute("value","");
     value=""
 });
+equal.addEventListener("click",()=>{    
+    if(value=="")return
+    let result="";
+    try{
+        result = math.evaluate(value);
+    }catch(e){
+        alert("Invalid Format");
+    }
+    if(result=="Infinity"){result="";alert("Division Error")}
+    input.setAttribute("value",result);
+    value=result
+    
+});
 input.addEventListener("keypress",(e)=>{
     if(e.key=="Enter"){
-        e.preventDefault();
-        equal.click();
+
     }
-});
-equal.addEventListener("click",()=>{
-    console.log(value);
-    input.setAttribute("value","");
-    
 });
 button.forEach(element => {
     if(element.textContent!="C" && element.textContent!="=")
@@ -26,3 +36,4 @@ button.forEach(element => {
         input.setAttribute('value',value)
     });
 });
+
