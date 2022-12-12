808792bbea51703213e33562146c04ed5995140e
Muneeb <muneeb.official.686332@hotmail.com>
2022-12-12 16:46:06 +0500

    Done

--- App.js
+++ App.js
@@ -2,36 +2,31 @@ let button = document.querySelectorAll(".items");
 let input = document.getElementById("input");
 let clear = document.getElementById("clear");
 let equal = document.getElementById("equal");
-let value=""
+let value = ""
 
 
-clear.addEventListener("click",()=>{
-    input.setAttribute("value","");
-    value=""
+clear.addEventListener("click", () => {
+    input.setAttribute("value", "");
+    value = ""
 });
-equal.addEventListener("click",()=>{    
-    if(value=="")return
-    let result="";
-    try{
+equal.addEventListener("click", () => {
+    if (value == "") return
+    let result = "";
+    try {
         result = math.evaluate(value);
-    }catch(e){
+    } catch (e) {
         alert("Invalid Format");
     }
-    if(result=="Infinity"){result="";alert("Division Error")}
-    input.setAttribute("value",result);
-    value=result
-    
-});
-input.addEventListener("keypress",(e)=>{
-    if(e.key=="Enter"){
+    if (result == "Infinity") { result = ""; alert("Division Error") }
+    input.setAttribute("value", result);
+    value = result
 
-    }
 });
 button.forEach(element => {
-    if(element.textContent!="C" && element.textContent!="=")
-    element.addEventListener("click",()=>{
-        value = value+element.textContent;
-        input.setAttribute('value',value)
-    });
+    if (element.textContent != "C" && element.textContent != "=")
+        element.addEventListener("click", () => {
+            value = value + element.textContent;
+            input.setAttribute('value', value)
+        });
 });
 
